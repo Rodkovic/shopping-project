41cef31eb225e64eaa5f22afb2aa73d83e98fae9
rodkovic <andrew.rodkovic@gmail.com>
2019-12-25 17:20:34 +0800

    项目初始化

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+    entry: path.join(__dirname, './src/main.js'),
+    output: {
+        path: path.join(__dirname, './dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        port: 3000,
+        open: true
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: path.join(__dirname, './src/index.html'),
+            filename: 'index.html'
+        }),
+        new VueLoaderPlugin()
+    ],
+    module: {
+        rules: [
+            { test: /\.vue$/, loader: 'vue-loader' }
+        ]
+    }
+}
